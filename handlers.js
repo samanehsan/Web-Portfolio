6160c5c87a266a64abd3ffefa63f7e2047ca8a34
vagrant <vagrant@precise32.(none)>
2014-02-17 20:41:50 +0000

    Heroku commit

---
+++ handlers.js
@@ -0,0 +1,11 @@
+exports.index = function(req, res) {
+	res.send("Welcome to CS1501");
+}
+
+exports.watch = function(req, res) {
+	var video_id = req.query.v;
+	res.render('watch', { id: video_id }, function(err, html) {
+		res.send(html);
+	})
+}
+
