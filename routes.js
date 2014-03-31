8e3e5dfa3c36cd76a1203dd5b41461e39f459b1e
vagrant <vagrant@precise32.(none)>
2014-03-31 19:40:17 +0000

    troubleshooting 2

--- routes.js
+++ routes.js
@@ -1,7 +1,7 @@
 module.exports = function(app) {
 	var handlers = require('./handlers');
 	app.get('/', handlers.index);
-	app.get('/', handlers.watch);
+	app.get('/watch', handlers.watch);
 	app.get('/bootstrap', handlers.bootstrap);
 	app.get('/pinterless', handlers.pinterless);
 
