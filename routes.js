b859bfe4a29daaceb9aca88a9fbcea4f02f556e7
vagrant <vagrant@precise32.(none)>
2014-04-21 20:22:51 +0000

    updated bootstrap

--- routes.js
+++ routes.js
@@ -2,7 +2,7 @@ module.exports = function(app) {
 	var handlers = require('./handlers');
 	app.get('/', handlers.index);
 	app.get('/watch', handlers.watch);
-	app.get('/home.html', handlers.bootstrap);
+	//app.get('/home.html', handlers.bootstrap);
 	app.get('/about.html', handlers.about);
 	app.get('/biochemistry.html', handlers.biochemistry);
 	app.get('/computerscience.html', handlers.cs);
